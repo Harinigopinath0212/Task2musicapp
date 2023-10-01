3c763f8dd3ce970a048a974e2c5b6f142bd74352
Harinigopinath0212 <146122056+Harinigopinath0212@users.noreply.github.com>
2023-10-01 14:28:36 +0100

    Add files via upload

---
+++ project intern 2/music-list.js
@@ -0,0 +1,29 @@
+
+
+let allMusic = [
+  {
+    name: "anbe anbe",
+    artist: "harish",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "kanave kanave",
+    artist: "anirudh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "naa ready",
+    artist: "anirudh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "nee partha",
+    artist: "swetha",
+    img: "music-4",
+    src: "music-4"
+  },
+
+];
